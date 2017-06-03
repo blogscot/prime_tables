e7497d98eeed57b66dfe4673700762f592118488
blogscot <iain.diamond@gmail.com>
2017-06-03 13:48:53 +0100

    Add find_primes

--- lib/prime_tables.ex
+++ lib/prime_tables.ex
@@ -4,7 +4,7 @@ defmodule PrimeTables do
   """
 
   @doc """
-  Reads an integer value from a file
+  Reads an integer value from a file.
   """
   def get_number(filename) do
     case File.read(filename) do
@@ -14,7 +14,7 @@ defmodule PrimeTables do
   end
 
   @doc """
-  Parses text into an integer value
+  Parses text into an integer value.
   """
   def parse_number(text) do
     case Integer.parse(text) do
@@ -24,7 +24,7 @@ defmodule PrimeTables do
   end
 
   @doc """
-  Returns true if the given integer value is prime
+  Returns true if the given integer value is prime.
 
   iex(1)> PrimeTables.is_prime?(100)
   false
@@ -40,4 +40,14 @@ defmodule PrimeTables do
     |> Enum.all?(&(&1 == false))
   end
 
+  @doc """
+  Returns a list of primes up to the given value.
+
+  iex(1)> PrimeTables.find_primes(50)
+  [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+  """
+
+  def find_primes(num) when is_integer(num) do
+    2..num |> Enum.filter(&PrimeTables.is_prime?/1)
+  end
 end
