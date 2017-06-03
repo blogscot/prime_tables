3d21ede548f989eaea613a58caf97a7bf4161d7d
blogscot <iain.diamond@gmail.com>
2017-06-03 15:45:28 +0100

    Add extra get_table test

--- test/prime_tables_test.exs
+++ test/prime_tables_test.exs
@@ -34,7 +34,18 @@ defmodule PrimeTablesTest do
 |    3 |    6 |    9 |   15 |
 |    5 |   10 |   15 |   25 |
 """
-
     assert expected == PrimeTables.get_table(6)
   end
+
+  test "multiplication table for integer ten" do
+    expected = """
+|      |    2 |    3 |    5 |    7 |
+|    2 |    4 |    6 |   10 |   14 |
+|    3 |    6 |    9 |   15 |   21 |
+|    5 |   10 |   15 |   25 |   35 |
+|    7 |   14 |   21 |   35 |   49 |
+"""
+    assert expected == PrimeTables.get_table(10)
+  end
+
 end
