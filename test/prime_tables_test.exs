ea214f2c77b47095d3f86520d12092bafa6ba1f4
blogscot <iain.diamond@gmail.com>
2017-06-03 12:43:03 +0100

    Add is_prime test

--- test/prime_tables_test.exs
+++ test/prime_tables_test.exs
@@ -2,15 +2,23 @@ defmodule PrimeTablesTest do
   use ExUnit.Case
 
   test "get a valid number" do
-    assert(PrimeTables.get_number("test/valid_number.txt") == {:ok, 20})
+    assert PrimeTables.get_number("test/valid_number.txt") == {:ok, 20}
   end
 
   test "get a number from missing file" do
-    assert(PrimeTables.get_number("test/invalid_number.txt") == {:error, "Error: invalid input."})
+    assert PrimeTables.get_number("test/invalid_number.txt") == {:error, "Error: invalid input."}
   end
 
   test "get an invalid number" do
-    assert(PrimeTables.get_number("missing_file.txt") == {:error, "Error: file not found."})
+    assert PrimeTables.get_number("missing_file.txt") == {:error, "Error: file not found."}
+  end
+
+  test "valid prime numbers are 2, 3, 5 ..." do
+    assert PrimeTables.is_prime(2)
+    refute PrimeTables.is_prime(6)
+    assert PrimeTables.is_prime(11)
+    refute PrimeTables.is_prime(15)
+    assert PrimeTables.is_prime(23)
   end
 
 end
